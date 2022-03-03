27c3569ed63e1d135ccb8b2fda5d19a9c5161bd6
J-45 <64985480+J-45@users.noreply.github.com>
2022-03-03 13:52:12 +0100

    fix week range + add function

--- get_data.py
+++ get_data.py
@@ -1,34 +1,50 @@
 #!/usr/bin/python3
 
-import os, gzip, shutil, urllib.request, datetime 
-from io import BytesIO
+import concurrent.futures
+import urllib.request
+import datetime
+import shutil
+import gzip
+import os
+import io
 
-url                 = 'https://tickdata.fxcorporate.com/'
+base_url            = 'https://tickdata.fxcorporate.com/'
 first_year          = 2018
 last_year           = datetime.datetime.now().year
-symbol_list         = ['AUDCAD','AUDCHF','AUDJPY', 'AUDNZD','CADCHF','EURAUD','EURCHF','EURGBP' 
-'EURJPY','EURUSD','GBPCHF','GBPJPY','GBPNZD','GBPUSD','GBPCHF','GBPJPY'
-'GBPNZD','NZDCAD','NZDCHF','NZDJPY','NZDUSD','USDCAD','USDCHF','USDJPY']
-error_list          = []
+# https://www.fxcm.com/markets/insights/anatomy-of-a-volatile-currency/#volatile-currencies-in-action
+symbol_list         = ['EURGBP', 'GBPCHF','GBPUSD']
+# symbol_list         = ['AUDCAD','AUDCHF','AUDJPY', 'AUDNZD','CADCHF','EURAUD','EURCHF','EURGBP' 
+# 'EURJPY','EURUSD','GBPCHF','GBPJPY','GBPNZD','GBPUSD','GBPCHF','GBPJPY'
+# 'GBPNZD','NZDCAD','NZDCHF','NZDJPY','NZDUSD','USDCAD','USDCHF','USDJPY']
+bad_url_list        = []
+
+def download(url, file_path):
+    try:
+        requests    = urllib.request.urlopen(url)
+        with open(file_path, 'wb') as local_file:
+            compressed = io.BytesIO(requests.read())
+            decompressed = gzip.GzipFile(fileobj=compressed)
+            shutil.copyfileobj(decompressed, local_file)
+        print(url)
+        return True
+    except urllib.error.HTTPError as exception:
+        print(url, exception)
+        return False
 
 for symbol in symbol_list:
     os.makedirs(symbol, exist_ok=True)
     for year in range(first_year, last_year + 1):
+        print(symbol, year)
         if year != last_year:
-            end_week = datetime.date(year,12,29).isocalendar()[1] # last week of the year
+            end_week    = datetime.date(year,12,29).isocalendar()[1] # last week of the year
         else:
-            end_week = datetime.datetime.now().isocalendar()[1] - 1 # get last week
-        for weeknumber in range(1, end_week):
-            url_data = url + symbol+'/'+str(year)+'/'+str(weeknumber)+'.csv.gz'
-            print(url_data)
-            try:
-                requests    = urllib.request.urlopen(url_data)
-                with open(f'{symbol}/{year}_{weeknumber}.csv', 'wb') as local_file:
-                    compressed = BytesIO(requests.read())
-                    decompressed = gzip.GzipFile(fileobj=compressed)
-                    shutil.copyfileobj(decompressed, local_file)
-            except urllib.error.HTTPError as exception:
-                error_list.append(url_data)
-                print(exception)
+            end_week    = datetime.datetime.now().isocalendar()[1] - 2 # two week before today
+        for weeknumber in range(1, end_week + 1):
+            data_url    = base_url + symbol+'/'+str(year)+'/'+str(weeknumber)+'.csv.gz'
+            file_path   = f'{symbol}/{year}_{weeknumber}.csv'
+            if not download(data_url, file_path):
+                bad_url_list.append(data_url)
 
-print(f'Missing:\n{error_list}')
+print("\nBad urls:")
+for bad_url in bad_url_list:
+    print(bad_url)
