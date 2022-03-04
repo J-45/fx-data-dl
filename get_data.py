65c154c864920557864372de268349b4a65bf533
J-45 <64985480+J-45@users.noreply.github.com>
2022-03-04 10:50:11 +0100

    + parallelism

--- get_data.py
+++ get_data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from socket import error as SocketError
 import concurrent.futures
 import urllib.request
 import datetime
@@ -8,43 +9,51 @@ import gzip
 import os
 import io
 
-base_url            = 'https://tickdata.fxcorporate.com/'
-first_year          = 2018
-last_year           = datetime.datetime.now().year
+NUMBER_OF_THREADS   = 10
+BASE_URL            = 'https://tickdata.fxcorporate.com/'
+FIRST_YEAR          = 2018
+LAST_YEAR           = datetime.datetime.now().year
 # https://www.fxcm.com/markets/insights/anatomy-of-a-volatile-currency/#volatile-currencies-in-action
-symbol_list         = ['EURGBP', 'GBPCHF','GBPUSD']
-# symbol_list         = ['AUDCAD','AUDCHF','AUDJPY', 'AUDNZD','CADCHF','EURAUD','EURCHF','EURGBP' 
+SYMBOL_LIST         = ['EURGBP', 'GBPCHF','GBPUSD']
+# SYMBOL_LIST         = ['AUDCAD','AUDCHF','AUDJPY', 'AUDNZD','CADCHF','EURAUD','EURCHF','EURGBP' 
 # 'EURJPY','EURUSD','GBPCHF','GBPJPY','GBPNZD','GBPUSD','GBPCHF','GBPJPY'
 # 'GBPNZD','NZDCAD','NZDCHF','NZDJPY','NZDUSD','USDCAD','USDCHF','USDJPY']
+url_count           = 0
 bad_url_list        = []
 
-def download(url, file_path):
+def download(url, FILE_PATH, bad_url_list):
     try:
         requests    = urllib.request.urlopen(url)
-        with open(file_path, 'wb') as local_file:
+        with open(FILE_PATH, 'wb') as local_file:
             compressed = io.BytesIO(requests.read())
             decompressed = gzip.GzipFile(fileobj=compressed)
             shutil.copyfileobj(decompressed, local_file)
         print(url)
-        return True
     except urllib.error.HTTPError as exception:
         print(url, exception)
-        return False
+        bad_url_list.append(url)
+    except ConnectionResetError as exception: # Catch "Connection reset by peer"
+        print(url, exception)
+        bad_url_list.append(url)
+    return bad_url_list
 
-for symbol in symbol_list:
-    os.makedirs(symbol, exist_ok=True)
-    for year in range(first_year, last_year + 1):
-        print(symbol, year)
-        if year != last_year:
-            end_week    = datetime.date(year,12,29).isocalendar()[1] # last week of the year
-        else:
-            end_week    = datetime.datetime.now().isocalendar()[1] - 2 # two week before today
-        for weeknumber in range(1, end_week + 1):
-            data_url    = base_url + symbol+'/'+str(year)+'/'+str(weeknumber)+'.csv.gz'
-            file_path   = f'{symbol}/{year}_{weeknumber}.csv'
-            if not download(data_url, file_path):
-                bad_url_list.append(data_url)
+with concurrent.futures.ThreadPoolExecutor(max_workers=NUMBER_OF_THREADS) as executor:
+    futures = []
+    for SYMBOL in SYMBOL_LIST:
+        os.makedirs(SYMBOL, exist_ok=True)
+        for YEAR in range(FIRST_YEAR, LAST_YEAR + 1):
+            if YEAR != LAST_YEAR:
+                END_WEEK    = datetime.date(YEAR,12,29).isocalendar()[1] # last week of the year
+            else:
+                END_WEEK    = datetime.datetime.now().isocalendar()[1] - 2 # two week before today
+            for weeknumber in range(1, END_WEEK + 1):
+                DATA_URL    = BASE_URL + SYMBOL+'/'+str(YEAR)+'/'+str(weeknumber)+'.csv.gz'
+                FILE_PATH   = f'{SYMBOL}/{YEAR}_{weeknumber}.csv'
+                futures.append(executor.submit(download, url=DATA_URL,FILE_PATH=FILE_PATH,bad_url_list=bad_url_list))
+                url_count   +=1
+    for future in concurrent.futures.as_completed(futures):
+        bad_url_list = future.result()
 
-print("\nBad urls:")
+print(f"\n{len(bad_url_list)} bad urls out of {url_count}:")
 for bad_url in bad_url_list:
     print(bad_url)
