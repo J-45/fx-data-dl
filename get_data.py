95260e571bcb501631461ad4c3e509b5e4dd4053
J-45 <64985480+J-45@users.noreply.github.com>
2022-03-07 16:51:11 +0100

    Unpack toggle

--- get_data.py
+++ get_data.py
@@ -8,7 +8,7 @@ import gzip
 import os
 import io
 
-NUMBER_OF_THREADS   = 10
+NUMBER_OF_THREADS   = 10 # Good enough for 50 Mo/s (400 Mb/s) bandwidth
 BASE_URL            = 'https://tickdata.fxcorporate.com/'
 FIRST_YEAR          = 2018
 LAST_YEAR           = datetime.datetime.now().year
@@ -19,24 +19,30 @@ SYMBOL_LIST         = ['EURGBP', 'GBPCHF','GBPUSD']
 # 'GBPNZD','NZDCAD','NZDCHF','NZDJPY','NZDUSD','USDCAD','USDCHF','USDJPY']
 url_count           = 0
 bad_url_list        = []
+UNPACK              = False
 
-def download(url, FILE_PATH, bad_url_list):
+def download(URL, file_path, bad_url_list):
     try:
-        requests    = urllib.request.urlopen(url)
-        with open(FILE_PATH, 'wb') as local_file:
-            compressed = io.BytesIO(requests.read())
-            decompressed = gzip.GzipFile(fileobj=compressed)
-            shutil.copyfileobj(decompressed, local_file)
-        print(url)
-    except urllib.error.HTTPError as exception:
-        print(url, exception)
-        bad_url_list.append(url)
-    except ConnectionResetError as exception: # Catch "Connection reset by peer"
-        print(url, exception)
-        bad_url_list.append(url)
+        REQUESTS    = urllib.request.urlopen(URL)
+        if UNPACK:
+            with open(file_path, 'wb') as Local_file:
+                COMPRESSED = io.BytesIO(REQUESTS.read())
+                DECOMPRESSED = gzip.GzipFile(fileobj=COMPRESSED)
+                shutil.copyfileobj(DECOMPRESSED, Local_file)
+        else:
+            with open(file_path+".gz", 'wb') as Local_file:
+                shutil.copyfileobj(io.BytesIO(REQUESTS.read()), Local_file)
+                
+        print(URL)
+    except urllib.error.HTTPError as Exception:
+        print(URL, Exception)
+        bad_url_list.append(URL)
+    except ConnectionResetError as Exception: # Catch "Connection reset by peer"
+        print(URL, Exception)
+        bad_url_list.append(URL)
     return bad_url_list
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=NUMBER_OF_THREADS) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=NUMBER_OF_THREADS) as Executor:
     futures = []
     for SYMBOL in SYMBOL_LIST:
         os.makedirs(SYMBOL, exist_ok=True)
@@ -45,14 +51,14 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=NUMBER_OF_THREADS) as exe
                 END_WEEK    = datetime.date(YEAR,12,29).isocalendar()[1] # last week of the year
             else:
                 END_WEEK    = datetime.datetime.now().isocalendar()[1] - 2 # two week before today
-            for weeknumber in range(1, END_WEEK + 1):
-                DATA_URL    = BASE_URL + SYMBOL+'/'+str(YEAR)+'/'+str(weeknumber)+'.csv.gz'
-                FILE_PATH   = f'{SYMBOL}/{YEAR}_{weeknumber}.csv'
-                futures.append(executor.submit(download, url=DATA_URL,FILE_PATH=FILE_PATH,bad_url_list=bad_url_list))
+            for WEEKNUMBER in range(1, END_WEEK + 1):
+                DATA_URL    = BASE_URL + SYMBOL+'/'+str(YEAR)+'/'+str(WEEKNUMBER)+'.csv.gz'
+                FILE_PATH   = f'{SYMBOL}/{YEAR}_{WEEKNUMBER}.csv'
+                futures.append(Executor.submit(download, URL=DATA_URL,file_path=FILE_PATH,bad_url_list=bad_url_list))
                 url_count   +=1
-    for future in concurrent.futures.as_completed(futures):
-        bad_url_list = future.result()
+    for FUTURE in concurrent.futures.as_completed(futures):
+        bad_url_list = FUTURE.result()
 
 print(f"\n{len(bad_url_list)} bad urls out of {url_count}:")
-for bad_url in bad_url_list:
-    print(bad_url)
+for BAD_URL in bad_url_list:
+    print(BAD_URL)
